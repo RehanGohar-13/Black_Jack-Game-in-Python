d2e7107f790ee923728b1f3e92ab27393c4ace33
Ateeq Rehman <221827389+RehanGohar-13@users.noreply.github.com>
2026-05-26 02:18:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import random
+
+name = "Iwen"
+print(name)
+age = 5000000
+print(age)
+print(type(name) == str)
+print(type(age) == int)
+#This is a comment
+
+name += " is a great person"
+print(name)
+print("""Hello
+World
+!!!""")
+
+print("Iwen".upper())
+print("Iwen".lower())
+print("Iwen is a great person.".title())
+print("Iwen is a great person.".islower())
+print("Iwen is a great person.".isupper())
+print(len("Iwen is a great person."))
+print("I" in name)
