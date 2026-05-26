0a6d05930db1519ae8b918c6bee024a7dcc80278
Ateeq Rehman <221827389+RehanGohar-13@users.noreply.github.com>
2026-05-26 16:09:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,2 @@
 import random
-
-name = "Iwen"
-print(name)
-age = 5000000
-print(age)
-print(type(name) == str)
-print(type(age) == int)
-#This is a comment
-
-name += " is a great person"
-print(name)
-print("""Hello
-World
-!!!""")
-
-print("Iwen".upper())
-print("Iwen".lower())
-print("Iwen is a great person.".title())
-print("Iwen is a great person.".islower())
-print("Iwen is a great person.".isupper())
-print(len("Iwen is a great person."))
-print("I" in name)
+from enum import Enum
